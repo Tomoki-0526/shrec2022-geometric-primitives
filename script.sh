f1f111e1c5167ef9cba19247e10f43ee4ac5c22d
Tomoki-0526 <eusford_0526@outlook.com>
2025-01-04 15:32:32 +0800

    修改路径

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@ DATASET_PATH='/home/szj/SHREC2022/dataset/test/pointCloud'
 
 for i in $(find $DATASET_PATH -name '*.txt');
 do
-    python evaluation.py --file=$i --outf=./output
+    python evaluation.py --file=$i --outf=/home/szj/SHREC2022/results/methods/M7/prediction_results
 done

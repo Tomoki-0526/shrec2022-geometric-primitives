a5d0e4df32db75a427f5522f4f92a1311a7fef79
Tomoki-0526 <eusford_0526@outlook.com>
2025-01-07 10:11:56 +0800

    修改script

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DATASET_PATH='/home/szj/SHREC2022/dataset/test/pointCloud'
+OUTPUT_DIR='/home/szj/SHREC2022/results/methods/M7/prediction_results'
 
 for i in $(find $DATASET_PATH -name '*.txt');
 do
-    python evaluation.py --file=$i --outf=/home/szj/SHREC2022/results/methods/M7/prediction_results
+    python evaluation.py --file=$i --outf=${OUTPUT_DIR}
 done
